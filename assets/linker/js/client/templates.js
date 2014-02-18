03ada30ee5c18bf0a0d8b3a139208867f74d1077
Jason Hooten <jason.hooten@gmail.com>
2014-02-18 09:45:47 -0600

    fixed some templates and added tabs

--- assets/linker/js/client/templates.js
+++ assets/linker/js/client/templates.js
@@ -4,6 +4,6 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   
 
 
-  data.buffer.push("<h1>Template</h1>");
+  data.buffer.push("\r\n<div class=\"row\">\r\n    <ul class=\"nav nav-tabs\" style=\"margin-bottom: 15px;\">\r\n        <li class=\"active\">\r\n            <a href=\"#\">Summary</a></li>\r\n        <li>\r\n            <a href=\"#attack\">Attack Stats</a></li>\r\n        <li>\r\n            <a href=\"#health\">Health Stats</a></li>\r\n        <li>\r\n            <a href=\"#mana\">Mana Stats</a></li>\r\n        <li>\r\n            <a href=\"#spell\">Spell Stats</a></li>\r\n    </ul>\r\n</div>");
   
 });
